7eb02521d15faa70375cdccce5eadc2643db54cf
Yung-Che <senseye935@gmail.com>
2024-08-31 18:55:23 +0800

    practice angular components and templates

---
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -0,0 +1,52 @@
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  EventEmitter,
+  OnInit,
+  output,
+  Output,
+  viewChild,
+  ViewChild,
+} from '@angular/core';
+import { ButtonComponent } from '../../../shared/button/button.component';
+import { ControlComponent } from '../../../shared/control/control.component';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-new-ticket',
+  standalone: true,
+  imports: [ButtonComponent, ControlComponent, FormsModule],
+  templateUrl: './new-ticket.component.html',
+  styleUrl: './new-ticket.component.css',
+})
+export class NewTicketComponent implements OnInit, AfterViewInit {
+  @ViewChild('form') private form?: ElementRef<HTMLFormElement>;
+  // private form = viewChild.required<ElementRef<HTMLFormElement>>('form');
+  // @Output() add = new EventEmitter<{ title: string; text: string }>();
+  enteredTitle = '';
+  enteredText = '';
+  add = output<{ title: string; text: string }>();
+
+  ngOnInit() {
+    console.log('ONINIT');
+    console.log(this.form?.nativeElement);
+  }
+
+  ngAfterViewInit(): void {
+    console.log('AFTER VIEW INIT');
+    console.log(this.form?.nativeElement);
+  }
+
+  // onSubmit(title: string, ticketText: string) {
+  //   this.add.emit({ title, text: ticketText });
+  //   this.form?.nativeElement.reset();
+  // }
+
+  onSubmit() {
+    this.add.emit({ title: this.enteredTitle, text: this.enteredText });
+    // this.form?.nativeElement.reset();
+    this.enteredText = '';
+    this.enteredTitle = '';
+  }
+}
